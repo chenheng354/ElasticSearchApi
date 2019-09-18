f0eb3865a7baffbd5d512d91dfc61a4b49f976cd
chenheng354 <15291198436@sina.cn>
2019-09-18 17:39:46 +0800

    Revert "杨鑫更改"
    This reverts commit cb743f1436b3bff142c22c3cfd82e32d8682b49c.

--- ElasticSearchApi/src/main/java/com/leyou/ElasticSearchApi.java
+++ ElasticSearchApi/src/main/java/com/leyou/ElasticSearchApi.java
@@ -50,10 +50,6 @@ public class ElasticSearchApi  {
     @After
     public void end(){
         client.close();
-    } 
-	@test
-    public void yangxin(){
-        client.close();
     }
 	@test
     public void anyongbo(){
